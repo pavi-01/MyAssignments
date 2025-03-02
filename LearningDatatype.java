7afb033d232da646c6ba78d01f48e2394aaf2375
pavi-01 <41281139+pavi-01@users.noreply.github.com>
2025-03-02 13:31:28 +0530

    Add files via upload
    day1 assignments

---
+++ LearningDatatype.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class LearningDatatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int regno=345678;
+		boolean isacar=true;
+		float speed=0.1345667467f;
+		char logo='N';
+		String name="Nexon";
+		long phno=8883543316l;
+		double capacity=123.1234567890123;
+		System.out.println("regno is:"+" "+ +regno);
+		System.out.println("Logo is:"+" "+logo);
+		System.out.println("Name of the car is"+" "+name);
+		System.out.println("Phone Number is"+phno);
+		System.out.println("The speed of the car is"+" "+speed);
+		System.out.println("Capacity"+" "+capacity);
+		System.out.println("Is a car"+" "+isacar);
+	}
+
+}
